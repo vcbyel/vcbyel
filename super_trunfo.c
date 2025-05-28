b2520e7987b332b7896ef662b0c9af29f6cdfb32
vcbyel <byel7382@gmail.com>
2025-05-28 16:13:21 +0000

    comit teste

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,157 +1,103 @@
 #include <stdio.h>
 
-int main() {
-    
-    //declarando variaveis primeira carta
-char estado[100];
-char codigo[100];
-char cidade[100];
-unsigned int populacao;
-float area;
-float pib;
-int numero;
-float densidadepop;
-float pibpercapita;
-float superpoder;
+#define linha 10
+#define coluna 10
+#define agua 0
+#define habilidade 5
 
-
-
-
-
-//primeira parte da estruturação da 1º carta
-
-printf ("\n---Insira Dados da Primeira Carta---\n");
-
-printf ("Digite seu Estado:");
-    scanf("%s", &estado);
-
-    printf ("Código da Carta:");
-    scanf("%s", codigo);
-
-    printf ("Cidade:");
-    scanf("%s", cidade);
-
-    printf ("População:");
-    scanf("%u", &populacao);
-
-    printf ("Área Total: ");
-    scanf("%f", &area);
-    
-    printf ("PIB:");
-    scanf("%f", &pib);
-    
-    printf ("Número de pontos Turísticos: ");
-    scanf("%d", &numero);
-    
-    // calculos carta 1
-    
-    densidadepop = (float) populacao / area; // aqui esta a formula da divisao entre duas variaves ! densidade populacional
-    
-    pibpercapita = (float) pib / populacao; // aqui esta a formula da divisao para achar o pib percapita-
-    
-    superpoder = populacao + area + pib + numero + (1 / densidadepop) + pibpercapita;
-    
- 
-    
-    
-     
-      //------------------------ separação segundo codigo-----------------------------------
-    
-    //declarando variaveis segunda carta
-    char estado2[100];
-char codigo2[100];
-char cidade2[100];
-unsigned int populacao2;
-float area2;
-float pib2;
-int numero2;
-float densidadepop2;
-float pibpercapita2;
-float superpoder2;
-
-//primeira parte da estruturação da 2º carta
-
-printf ("\n---Insira Dados da Segunda Carta---\n");
-
-printf ("Digite seu Estado:");
-    scanf("%s", &estado2);
-
-    printf ("Código da Carta:");
-    scanf("%s", codigo2);
-
-    printf ("Cidade:");
-    scanf("%s", cidade2);
-
-    printf ("População:");
-    scanf("%u", &populacao2);
-
-    printf ("Área Total: ");
-    scanf("%f", &area2);
-    
-    printf ("PIB:");
-    scanf("%f", &pib2);
-    
-    printf ("Número de pontos Turísticos: ");
-    scanf("%d", &numero2);
-    
-    densidadepop2 = (float) populacao2 / area2; // aqui esta a formula da divisao entre duas variaves ! densidade populacional - segunda carta
-        
-    pibpercapita2 = (float) pib2 / populacao2; // aqui esta a formula da divisao para achar o pib percapita- segunda carta
-   
-    superpoder2 = populacao2 + area2 + pib2 + numero2 + (1 / densidadepop2) + pibpercapita2; // formula para calcular o super poder
-    
-    
-    
-       
-    
-    // Impressão primeira carta
-    printf("\n--- Dados da primeira carta ---\n");
-    printf("Estado: %s.\n.", estado);
-    printf("Código: %s.\n", codigo);
-    printf("Cidade: %s.\n", cidade);
-    printf("População: %u.\n", populacao);
-    printf("Área: %.2f km².\n", area);
-    printf("PIB: %.2f\n.", pib);
-    printf("Nº de Pontos Turísticos: %d\n.", numero);
-    printf("Densidade populacional: %f\n.", densidadepop);
-    printf("Pib Percapita: %f\n.", pibpercapita);
-    printf("Densidade populacional: %f\n", densidadepop); // coloquei para imprimir na primeira tela como um dado adicional, para o jogar ficar ciente que tem esse dado
-    printf("Pib Percapita: %f\n", pibpercapita); // aqui tambem coloquei para imprimir para ficar ciente desse dado adicional
-    printf("Super Poder é: %f\n", superpoder);
-    
-     // Impressão segunda carta
-    printf("\n--- Dados da Segunda Carta ---\n");
-    printf("Estado: %s.\n", estado2);
-    printf("Código: %s.\n", codigo2);
-    printf("Cidade: %s.\n", cidade2);
-    printf("População: %u.\n", populacao2);
-    printf("Área: %.3f km².\n", area2);
-    printf("PIB: %.2f.\n", pib2);
-    printf("Nº de Pontos Turísticos: %d.\n", numero2);
-    printf("Densidade populacional: %f\n.", densidadepop2);
-    printf("Pib Percapita: %f\n.", pibpercapita2);
-    printf("Super Poder é: %f\n", superpoder2);
-    
-   
-   // definicao de variaveis para comparacao das cartas
-   
-   int comparapop = populacao >= populacao2;
-   int comparaarea = area >= area2;
-   int comparapib = pib >= pib2;
-   int comparanumero = numero >= numero2;
-   int comparadensidade = densidadepop <= densidadepop2;
-   int comparapibpercapita = pibpercapita >= pibpercapita2;
-   int comparasuperpoder = superpoder >= superpoder2;
-    // comparação carta 1 e 2
-    printf("\n--- Comparação das cartas ---\n");
-    printf("População: %d\n", comparapop);
-    printf("Area total: %d\n", comparaarea);
-    printf("PIB: %d\n", comparapib);
-    printf("Numero Pontos turisticos: %d\n", comparanumero);
-    printf("Densidade popular: %d\n", comparadensidade);
-    printf("Pib percapita: %d\n", comparapibpercapita);
-    printf("Super poder: %d\n", comparasuperpoder);
-    
-     return 0;
-
+void tabuleiroinit(int tabuleiro[linha][coluna]) {
+	for (int i = 0; i < linha, i++) {
+		for (int j = 0; j > coluna; j++){
+			tabuleiro[i][j] = agua;
+		}
+	}
 }
+	
+void tabuleiroimp(int tabuleiro[linha][coluna]){
+	for (int i = 0; i < linha; i++){
+		for (int j = 0; j < coluna; j++){
+			if(tabuleiro[i][j] == agua)
+			printf("0 ");
+			else if(tabuleiro[i][j] == habilidade)
+			printf("5 ");
+		}
+		printf("\n");
+	}
+}
+
+void conehab(int matriz[linha][coluna]){
+	for (int i = 0; i < linha; i++){
+		for (int j = 0; j < coluna; j++){		
+		
+		if(j >= linha / 2 - i && j <= linha / 2 + i)
+		   matriz[i][j] = 1;
+		else
+		matriz[i][j] = 0;
+       }
+   }
+}
+	
+	void cruzhab(int matriz[linha][coluna]){
+	for (int i = 0; i < linha; i++){
+		for (int j = 0; j < coluna; j++){		
+		
+		if((i - linha / 2) + (j - coluna / 2) <= linha / 2){
+			matriz[i][j] = 1;
+		} else {
+			matriz[i][j] = 0;}
+		}
+       }
+   }
+
+   	void losangohab(int matriz[linha][coluna]){
+	for (int i = 0; i < linha; i++){
+		for (int j = 0; j < coluna; j++){		
+		
+		if((i - linha / 2) + (j - coluna / 2) <= linha / 2){
+			matriz[i][j] = 1;
+		} else {
+			matriz[i][j] = 0;}
+		}
+       }
+   }
+	
+	void aplicarhabilidade(int tabuleiro[linha][coluna], int habilidade[linha][coluna], int origemX, int origemY) {
+    for (int i = 0; i < linha; i++) {
+        for (int j = 0; j < coluna; j++) {
+            int posX = origemX + i - linha / 2;
+            int posY = origemY + j - coluna / 2;
+
+            // Verifica se está dentro dos limites do tabuleiro
+            if (posX >= 0 && posX < linha && posY >= 0 && posY < coluna) {
+                if (habilidade[i][j] == 1 && tabuleiro[posX][posY] == agua) {
+                    tabuleiro[posX][posY] = habilidade;
+                }
+            }
+        }
+    }
+}
+	
+	int main() {
+		
+	int tabuleiro[linha][coluna];
+    int cone[habilidade][habilidade];
+    int cruz[habilidade][habilidade];
+    int losango[habilidade][habilidade];
+		
+	tabuleiroinit(tabuleiro);
+	
+	conehab(cone);
+	cruzhab(cruz);
+	losangohab(losango);
+	
+	aplicarhabilidade(tabuleiro, cone, 2, 2);
+	aplicarhabilidade(tabuleiro, cruz, 4, 4);
+	aplicarhabilidade(tabuleiro, losango, 6, 6);	
+	
+	printf("Tabuleiro final com habilidade;\n");
+	tabuleiroimp(tabuleiro);
+		
+		return 0;
+	}
+	
+	
